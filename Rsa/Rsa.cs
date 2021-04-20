53fdaf1188630b445112076f74ec1777131ca530
Andrey <kouznts@ya.ru>
2021-04-20 13:11:36 +0400

    def is prime

--- Rsa/Rsa.cs
+++ Rsa/Rsa.cs
@@ -1,4 +1,4 @@
-﻿namespace Rsa
+namespace Rsa
 {
     public class Rsa
     {
@@ -12,5 +12,14 @@
             'Ъ', 'Э', 'Ю', 'Я', ' ',
             '1', '2', '3', '4', '5',
             '6', '7', '8', '9', '0' };
+
+        public static bool IsPrime(long num)
+        {
+            for (long i = 2; i <= Math.Sqrt(num); i++)
+                if (num % i == 0)
+                    return false;
+
+            return true;
     }
+
 }
