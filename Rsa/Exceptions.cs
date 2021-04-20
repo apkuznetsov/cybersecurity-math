7c2f42952f7ec302db2b85d6afce5841ce0804df
Andrey <kouznts@ya.ru>
2021-04-20 14:07:02 +0400

    def not positive exception

--- Rsa/Exceptions.cs
+++ Rsa/Exceptions.cs
@@ -8,4 +8,11 @@ namespace Rsa
         public NotPrimeException(string message) : base(message) { }
         public NotPrimeException(string message, Exception inner) : base(message, inner) { }
     }
+
+    public class NotPositiveException : Exception
+    {
+        public NotPositiveException() { }
+        public NotPositiveException(string message) : base(message) { }
+        public NotPositiveException(string message, Exception inner) : base(message, inner) { }
+    }
 }
