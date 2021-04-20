a95afcf682f02f1bba36743732b8d2b0886d95fc
Andrey <kouznts@ya.ru>
2021-04-20 12:08:39 +0400

    def not prime exception

---
+++ Rsa/Exceptions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Rsa
+{
+    public class NotPrimeException : Exception
+    {
+        public NotPrimeException() { }
+        public NotPrimeException(string message) : base(message) { }
+        public NotPrimeException(string message, Exception inner) : base(message, inner) { }
+    }
+}
