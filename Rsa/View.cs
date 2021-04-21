0e5ab7e855172ab7abde6c240254027e63d4a89a
Andrey <kouznts@ya.ru>
2021-04-21 12:27:25 +0400

    def click encrypt button

--- Rsa/View.cs
+++ Rsa/View.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Windows.Forms;
@@ -11,5 +11,22 @@ namespace Rsa
         {
             InitializeComponent();
         }
+
+        private void btnEncrypt_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                long p = Convert.ToInt64(nudP.Text);
+                long q = Convert.ToInt64(nudQ.Text);
+                long d;
+                long n;
+
+                RsaText.Encrypt(p, q, out d, out n);
+                nudD.Value = d;
+                nudN.Value = n;
     }
+            catch
+            {
+                MessageBox.Show("НЕИЗВЕСТНАЯ ОШИБКА");
 }
+        }
