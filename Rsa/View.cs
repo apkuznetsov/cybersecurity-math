0098f221b8a77fe8b5a4355d4d739d8ed3d3d5ef
Andrey <kouznts@ya.ru>
2021-04-21 12:25:30 +0400

    def view class template

---
+++ Rsa/View.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Rsa
+{
+    public partial class View : Form
+    {
+        public View()
+        {
+            InitializeComponent();
+        }
+    }
+}
