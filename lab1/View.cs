67c64abfa56c3dd9468802d78e830aa54e9087f7
apkuznetsov <kouznts@ya.ru>
2021-05-15 15:57:37 +0400

    click encode button

--- lab1/View.cs
+++ lab1/View.cs
@@ -17,5 +17,13 @@ namespace lab1
             InitializeComponent();
         }
 
+        private void btnEncode_Click(object sender, EventArgs e)
+        {
+            tbKeyseq1.Text = VernamCipher.GenerateKeyseq(tbOpenText.Text.Length);
+            tbEncodedText1.Text = VernamCipher.Encode(tbOpenText.Text, tbKeyseq1.Text);
+            
+            tbKeyseq2.Text = tbKeyseq1.Text;
+            tbEncodedText2.Text = tbEncodedText1.Text;
+        }
     }
 }
