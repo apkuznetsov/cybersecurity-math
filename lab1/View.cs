8f62ffb2dd2047f98414e5a9daaf369961f7a954
apkuznetsov <kouznts@ya.ru>
2021-05-15 18:10:23 +0400

    rename UI elements

--- lab1/View.cs
+++ lab1/View.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace lab1
@@ -17,13 +10,18 @@ namespace lab1
             InitializeComponent();
         }
 
-        private void btnEncode_Click(object sender, EventArgs e)
+        private void btnEncrypt_Click(object sender, EventArgs e)
         {
             tbKeyseq1.Text = VernamCipher.GenerateKeyseq(tbOpenText.Text.Length);
-            tbEncodedText1.Text = VernamCipher.Encode(tbOpenText.Text, tbKeyseq1.Text);
-            
+            tbEncryptedText1.Text = VernamCipher.Encrypt(tbOpenText.Text, tbKeyseq1.Text);
+
             tbKeyseq2.Text = tbKeyseq1.Text;
-            tbEncodedText2.Text = tbEncodedText1.Text;
+            tbEncryptedText2.Text = tbEncryptedText1.Text;
+        }
+
+        private void btnDecrypt_Click(object sender, EventArgs e)
+        {
+            tbDecryptedText.Text = VernamCipher.Decrypt(tbEncryptedText2.Text, tbKeyseq2.Text);
         }
     }
 }
